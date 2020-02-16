d712883fcdb4d0bea9bdb5ea08aed2f4b0bf5e63
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-16 17:16:23 -0600

    Update parallel_forecast()

--- R/forecast.R
+++ R/forecast.R
@@ -11,20 +11,22 @@
 #' @examples
 parallel_forecast <- function(.mbl, ...) {
 
-  # If not a list of mables, split the data
-  if (is_mable(.mbl)) {
-    workers <- future::nbrOfWorkers()
-    num_keys <- tsibble::n_keys(.mbl)
+  workers <- future::nbrOfWorkers()
+  num_keys <- tsibble::n_keys(.mbl)
 
-    if (num_keys < workers)
-      splits <- num_keys
-    else
-      splits <- workers
+  if (num_keys < workers)
+    splits <- num_keys
+  else
+    splits <- workers
 
-    .mbl <- .mbl %>%
-      mutate(group_id = row_number() %% splits) %>%
-      group_split(group_id, keep = FALSE)
-  }
+  mable_names <- names(.mbl)
+  mable_keys <- key_vars(.mbl)
+  mable_models <- mable_names[!mable_names %in% mable_keys]
+
+  .mbl <- .mbl %>%
+    mutate(group_id = row_number() %% splits) %>%
+    group_split(group_id, keep = FALSE) %>%
+    map(as_mable, key = all_of(mable_keys), models = all_of(mable_models))
 
   results <- suppressWarnings(
     future.apply::future_lapply(.mbl, fabletools::forecast, ...) %>%
--- R/parable_package.R
+++ R/parable_package.R
@@ -1,6 +1,6 @@
 # Suppress R CMD check note
 #' @importFrom dplyr bind_rows group_indices group_split mutate row_number select
-#' @importFrom fabletools is_mable
+#' @importFrom fabletools as_mable is_mable
 #' @importFrom future.apply future_lapply
 #' @importFrom rlang is_formula parse_expr
 #' @importFrom tidyselect all_of
