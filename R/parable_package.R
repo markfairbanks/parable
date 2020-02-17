a31e9afee8685864f2d49eb5d10ca6c1834ce569
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-16 19:15:14 -0600

    Update NAMESPACE

--- R/parable_package.R
+++ R/parable_package.R
@@ -2,7 +2,7 @@
 #' @importFrom dplyr bind_rows group_indices group_split mutate row_number select ungroup
 #' @importFrom fabletools as_mable is_mable
 #' @importFrom future.apply future_lapply
-#' @importFrom rlang abort enexprs is_formula parse_expr
+#' @importFrom rlang abort enexprs is_formula is_list parse_expr
 #' @importFrom tidyselect all_of
 #' @importFrom tsibble group_by_key is_tsibble key_vars n_keys
 #' @importFrom vctrs new_vctr
