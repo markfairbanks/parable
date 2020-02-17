1d733734e5ebe47164b4e4da2c91e9951155b0fd
Mark Fairbanks <mark.t.fairbanks@gmail.com>
2020-02-16 19:11:10 -0600

    Update imports & dependencies

--- R/parable_package.R
+++ R/parable_package.R
@@ -1,8 +1,9 @@
 # Suppress R CMD check note
-#' @importFrom dplyr bind_rows group_indices group_split mutate row_number select
+#' @importFrom dplyr bind_rows group_indices group_split mutate row_number select ungroup
 #' @importFrom fabletools as_mable is_mable
 #' @importFrom future.apply future_lapply
-#' @importFrom rlang is_formula parse_expr
+#' @importFrom rlang abort enexprs is_formula parse_expr
 #' @importFrom tidyselect all_of
-#' @importFrom tsibble group_by_key is_tsibble
+#' @importFrom tsibble group_by_key is_tsibble key_vars n_keys
+#' @importFrom vctrs new_vctr
 NULL
